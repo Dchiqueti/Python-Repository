59db58dcb9bd6b34ad3d4cc8f71f0cb01042a966
Davi Chiqueti <98603460+Dchiqueti@users.noreply.github.com>
2023-03-13 16:36:24 -0300

    Add files via upload
    This is practice code inspired in The Last Of Us
    I did it for train the While Loop, also used some libraries like random and time
    
    In the following link you can watch a demonstration of the code running on cmd
    https://www.youtube.com/watch?v=BeDiHUhYXHs

---
+++ TLOU.py
@@ -0,0 +1,51 @@
+import random
+import time
+from os import system
+
+system('cls')
+infected = int(input('Digite a quantidade de infectados que Joel e Ellie irão enfrentar: '))
+if infected >= 10:
+    
+    print('\nNossa!', infected, 'infectados, vai ser complicado')
+
+elif infected < 3:
+    
+    print('Só isso?', infected, 'é moleza')
+
+else: 
+    
+    print('\n', infected, 'infectados, é uma briga justa')
+
+time.sleep(2)
+while infected != 0:
+    
+    custom = random.randint(1,5)
+    if custom == 1 and infected >= 2:
+        
+        print('\n Joel dispersa um tiro de doze que mata dois infectados de uma vez!')
+        infected -= 2
+        
+    elif custom == 2:
+        
+        print('\n Joel está em apuros, um infectado está em cima dele!')
+        time.sleep(2)
+        print(' Ellie esfaqueia o infectado e ajuda Joel')
+        infected -= 1
+    
+    elif custom == 3 and infected <= 4:
+
+        print('\n Droga, chegou mais um!')
+        infected += 1
+
+    ## Definir 2 entre as 5 possibilidades para ser o evento com maior probabilidade
+    elif custom == 4 or custom == 5:
+        
+        print('\n Joel atira com a pistola em um infectado')
+        infected -=1
+    
+    else:
+        continue
+    print(' infectados restantes:', infected)
+    time.sleep(2)
+
+print('\n Finalmente acabou!')
